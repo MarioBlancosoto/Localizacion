8422c306bec14c2e96145020196f13bca36bffe9
Damian <damian@danielcastelao.org>
2018-01-14 11:28:34 +0100

    Incluimos el mapa, con la marca, los controles y la posición actual

--- app/src/main/java/com/ejemplos/gpslocation/locationactivity/LocationActivity.java
+++ app/src/main/java/com/ejemplos/gpslocation/locationactivity/LocationActivity.java
@@ -13,13 +13,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class LocationActivity extends FragmentActivity {
+public class LocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String TAG = "gpslog";
     private LocationManager mLocMgr;
     private TextView textViewGPS, label;
+
+    private GoogleMap mMap;
 
     //Minimo tiempo para updates en Milisegundos
     private static final long MIN_CAMBIO_DISTANCIA_PARA_UPDATES = (long) 5; // 5 metro
@@ -32,25 +41,32 @@ public class LocationActivity extends FragmentActivity {
         setContentView(R.layout.activity_location);
         textViewGPS = (TextView) findViewById(R.id.lat);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         mLocMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             //Requiere permisos para Android 6.0
             Log.e(TAG, "No se tienen permisos necesarios!, se requieren.");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 225);
             return;
-        }else{
+        } else {
             Log.i(TAG, "Permisos necesarios OK!.");
             // registra el listener para obtener actualizaciones
             mLocMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIEMPO_ENTRE_UPDATES, MIN_CAMBIO_DISTANCIA_PARA_UPDATES, locListener, Looper.getMainLooper());
         }
-        textViewGPS.setText("Lat " +    " Long " );
+        textViewGPS.setText("Lat " + " Long ");
 
     }
+
     // recibe notificaciones del LocationManager
     public LocationListener locListener = new LocationListener() {
         public void onLocationChanged(Location location) {
             Log.i(TAG, "Lat " + location.getLatitude() + " Long " + location.getLongitude());
-            textViewGPS.setText("Lat " +   location.getLatitude() + " Long " + location.getLongitude());
+            textViewGPS.setText("Lat " + location.getLatitude() + " Long " + location.getLongitude());
+
         }
 
         public void onProviderDisabled(String provider) {
@@ -65,4 +81,28 @@ public class LocationActivity extends FragmentActivity {
             Log.i(TAG, "onStatusChanged()");
         }
     };
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //engadimos controls
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        // Engadimos marca
+        LatLng darbo = new LatLng(42.263044, -8.802236);
+        mMap.addMarker(new MarkerOptions().position(darbo).title("Marca en Darbo"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(darbo));
+    }
 }
